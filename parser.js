b9d4076087d7aadcf0a5da161cf46c36bf03f271
Dody Herawan <dody@Dodys-MacBook-Pro.local>
2018-09-10 11:24:10 +0700

    Release 0, Release 0.5

--- parser.js
+++ parser.js
@@ -1,25 +1,50 @@
 "use strict"
 
+// inserting file
+var fs = require('fs')
+var nameList = fs.readFileSync('people.csv')
+  .toString()
+  .split("\n")
+
+// class person
 class Person {
   // Look at the above CSV file
   // What attributes should a Person object have?
+  constructor(input) {
+    let splitted = input.split(',');
+    this._id = splitted[0];
+    this._first_name = splitted[1];
+    this._last_name = splitted[2];
+    this._email = splitted[3];
+    this._phone = splitted[4];
+    this._created_at = splitted[5];
+  }
 }
 
+// class person
 class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = [];
+  }
+
+  rollPeople() {
+    for (let i = 1; i < this._file.length; i++) {
+      let dudette = new Person(nameList[i]);
+      this._people.push(dudette);
+    }
+    return this.people;
   }
 
   get people() {
     return this._people
   }
 
+  set people(input) {
+  }
+
   addPerson() {}
-
 }
-
-let parser = new PersonParser('people.csv')
-
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+let dudes = new PersonParser(nameList).rollPeople()
+console.log(dudes);
