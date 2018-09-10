87e918ec53b2c1a54020bc08add8ce49a9c0ce80
anastasia <anastasia.santoso.id@gmail.com>
2018-09-10 12:55:50 +0700

    Release 0

--- parser.js
+++ parser.js
@@ -3,23 +3,42 @@
 class Person {
   // Look at the above CSV file
   // What attributes should a Person object have?
+  constructor(people_list) {
+    this._id = people_list[0];
+    this._firstName = people_list[1];
+    this._lastName = people_list[2];
+    this._email = people_list[3];
+    this._phone = people_list[4];
+    this._createdAt = people_list[5];
+  }
 }
 
 class PersonParser {
 
   constructor(file) {
-    this._file = file
-    this._people = null
+    this._file = file;
+    this._people = [];
   }
 
-  get people() {
-    return this._people
+  people() {
+    for (var i = 1; i < this._file.length; i++) {
+      let arrayOfPeople = this._file[i].split(',');
+      let obj = new Person(arrayOfPeople);
+      this._people.push(obj);
+    }
+    return this._people;
   }
 
   addPerson() {}
 
 }
+var fs = require('fs');
+var people_list = fs.readFileSync('people.csv')
+  .toString()
+  .split("\n")
 
-let parser = new PersonParser('people.csv')
+let parser = new PersonParser(people_list);
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+console.log(parser.people());
+
+//console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
